e0eec1e038b482387a44ecaf57b5afbc4c7efa63
Tim108 <tsonderen108@gmail.com>
2017-05-24 00:41:09 +0200

    fixed to_internal_i()

--- matrix.cpp
+++ matrix.cpp
@@ -14,33 +14,17 @@ using namespace std;
 template<class T>
 class Matrix {
 private:
-    size_t m_size, m_capacity, m_dimension;
+    size_t m_size, m_capacity, m_dimension, m_size_internal;
     T *list;
 
-    const size_t to_internal_i(size_t i)const {
-        size_t i2 = i;
-        // first to two dimensional
-        size_t x = i2 % m_dimension;
-        size_t y = floor(i2 / m_dimension);
 
-        // swap if needed
-        if (x > y) {
-            size_t z = x;
-            x = y;
-            y = z;
-        }
-
-        // then back to one dimensional
-        i2 = x + (y * m_dimension);
-
-        return i2;
-    }
 
 public:
     explicit Matrix(size_t n = 0) {
         m_dimension = ceil(sqrt(n));
         m_size = n;
         m_capacity = m_dimension * m_dimension;
+        m_size_internal = 0;
         list = new T[((m_dimension * (m_dimension + 1)) / 2) * sizeof(T)];
         cout << "--" <<  m_dimension << "--" << m_size << "--" << m_capacity << "--" << endl;
 
@@ -51,6 +35,7 @@ public:
         m_dimension = m.m_dimension;
         m_size = m.m_size;
         m_capacity = m.m_capacity;
+        m_size_internal = m.m_size_internal;
         list = new T[((m_dimension * (m_dimension + 1)) / 2) * sizeof(T)];
         copy(m.cbegin(), m.cend(), list);
     }
@@ -59,6 +44,7 @@ public:
         m_dimension = m.m_dimension;
         m_size = m.m_size;
         m_capacity = m.m_capacity;
+        m_size_internal = m.m_size_internal;
         list = m.list;
         m.list = nullptr;
         m.m_size = 0;
@@ -75,6 +61,7 @@ public:
             m_dimension = m.m_dimension;
             m_size = m.m_size;
             m_capacity = m.m_capacity;
+            m_size_internal = m.m_size_internal;
             list = new T[((m_dimension * (m_dimension + 1)) / 2) * sizeof(T)];
             copy(m.cbegin(), m.cend(), list);
         }
@@ -87,6 +74,7 @@ public:
             m_dimension = m.m_dimension;
             m_size = m.m_size;
             m_capacity = m.m_capacity;
+            m_size_internal = m.m_size_internal;
             list = m.list;
             m.list = nullptr;
             // rrV.m_size = 0;    // better not to do this to be able to detect errors like (1)
@@ -95,28 +83,37 @@ public:
     }
 
     T &operator[](size_t i) {
-        return list[to_internal_i(i)];
+        return list[to_internal_i(m_dimension, i)];
     }
 
     const T &operator[](size_t i) const {
-        return list[to_internal_i(i)];
+        return list[to_internal_i(m_dimension, i)];
     }
 
-    void insert(const T &t){
+    void push_back(const T &t){
         if(m_size >= m_capacity){
+            cout << "=========================================" << endl;
+            output("before expanding");
             m_dimension++;
             m_capacity = m_dimension * m_dimension;
             T *newlist = new T[((m_dimension * (m_dimension + 1)) / 2) * sizeof(T)];
             for (size_t i = 0; i < m_size; ++i) {
-                newlist[to_internal_i(i)] = list[to_internal_i(i)];
+                newlist[to_internal_i(m_dimension, i)] = list[to_internal_i(m_dimension-1, i)];
             }
             swap(newlist, list);
 
             delete[] newlist;
             cout << "capacity increased to: " << m_capacity << endl;
+            output("after expanding");
+            cout << "==========================================" << endl;
         }
 
-        list[to_internal_i(m_size)] = t;
+//        cout << "before: " << m_size_internal << endl;
+//        list[m_size_internal] = t;
+//        m_size_internal++;
+//        cout << "after: " << m_size_internal << endl;
+
+        list[to_internal_i(m_dimension, m_size)] = t;
         if (m_size % m_dimension == floor(m_size / m_dimension)) {
             m_size += 1;
         } else{
@@ -183,24 +180,53 @@ public:
         for (int i = 0; i < m_capacity; ++i) {
             if(i % m_dimension == 0 && i != 0)
                 cout << endl;
-            cout << list[to_internal_i(i)] << '\t';
+            cout << list[to_internal_i(m_dimension, i)] << '\t';
         }
         cout << endl;
+    }
+
+    const size_t to_internal_i(size_t dim, size_t i)const {
+        size_t i2 = i;
+        // first to two dimensional
+        size_t x = i2 % dim;
+        size_t y = floor(i2 / dim);
+
+        // swap if needed
+        if (x > y) {
+            size_t z = x;
+            x = y;
+            y = z;
+        }
+
+        // then back to one dimensional
+//        i2 = x + (y * dim);
+
+        size_t i3 = 0;
+        size_t temp = 0;
+        for (int j = 0; j < y; ++j) {
+            i3 += ++temp;
+        }
+        i3 += x;
+        return i3;
     }
 };
 
 
 
 int main() {
-    Matrix<int> v(0);
-//    v.output("base");
-//    for (int i = 0; i < 10; ++i)
-//        v[i] = i;
-    v.output("one");
-    for (int i = 0; i < 24; ++i)
-        v.insert(i);
-    v.output("two");
-//    v.output("print dat shit");
+    Matrix<int> v(16);
+    v.output("base");
+    for (int i = 0; i < 16; ++i) {
+        v[i] = i;
+        v.output(to_string(i).c_str());
+    }
+
+
+//    v.output("before");
+//    for (int i = 0; i < 6; ++i) {
+//        v.push_back(i);
+//        v.output(to_string(i).c_str());
+//    }
 
 //    v.pop_front();
 //    v.output("v");
