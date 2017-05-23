da1c487fd5eea2ddb75ac4aef1df13d954c2711d
Tim108 <tsonderen108@gmail.com>
2017-05-23 15:43:41 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Metaprogramming0.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Metaprogramming0.dir/symm.cpp.o"
+  "CMakeFiles/Metaprogramming0.dir/Vector.cpp.o"
+  "CMakeFiles/Metaprogramming0.dir/matrix.cpp.o"
+  "Metaprogramming0.pdb"
+  "Metaprogramming0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Metaprogramming0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
